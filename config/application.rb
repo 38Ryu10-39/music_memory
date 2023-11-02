24b342e9b4a7a5f929ed614ffc6d7340d4cceb58
38Ryu10-39 <keshi-.-tp_38@docomo.ne.jp>
2023-11-02 20:05:59 +0900

    Add: 初期設定を行いました。gemをインストール、generateコマンドで自動生成されるファイルを制限、bootstrapとfontawesomeの適用、headerとfooterの

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,19 @@ module MusicMemory
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
+
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
