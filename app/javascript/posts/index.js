edaaa3aa2b58266d3a044d3a7244b6821e2f5fcf
38Ryu10-39 <keshi-.-tp_38@docomo.ne.jp>
2023-11-22 20:51:50 +0900

    Add: 投稿一覧ページでで日本地図をjmapを利用して実装しました

---
+++ app/javascript/posts/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $('#jmap').jmap({
+      width: '100%',
+      height: '450px',
+      lineColor: 'white',
+      lineWidth: 2,
+      showTextShadow: true,
+      backgroundColor: 'white',
+      backgroundRadius: '0.5rem',
+      backgroundPadding: '0.5rem',
+      dividerColor: '#EFE4A6',
+      showPrefectureName: true,
+      showRoundedPrefecture: true,
+      prefectureNameType: 'full',
+      prefectureBackgroundColor: '#62B34C',
+      prefectureBackgroundHoverColor: '#95A834',
+      prefectureLineColor: '#EFE4A6',
+      prefectureLineHoverColor: '#ffffff',
+      prefectureLineWidth: '2px',
+      prefectureLineGap: '0px',
+      prefectureInnerLineWidth: '1px',
+      prefectureInnerLineColor: '#EFE4A6',
+      prefectureInnerLineType: 'outset',
+      prefectureRadius: '15px',
+      prefectureLineY: '2px',
+      prefectureLineX: '2px',
+      fontSize: '0.6rem',
+      fontColor: '#fff'
+  });
+});
