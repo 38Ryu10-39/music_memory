69714189e5d877f076969f36df56a5cc2a173506
38Ryu10-39 <keshi-.-tp_38@docomo.ne.jp>
2023-12-01 20:27:35 +0900

    Fix: お問合せフォームに関して内容を変更しました.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,11 +5,11 @@ class ContactMailer < ApplicationMailer
 
   def send_mail(contact)
     @contact = contact
-    mail(from: contact.email, to: ENV['MAIL_ADDRESS'], subject: '【お問い合わせ】' + @contact.subject)
+    mail(from: @contact.email, to: ENV['MAIL_ADDRESS'], subject: '【お問い合わせ】' + @contact.subject)
   end
 
   def contact_mail(contact)
     @contact = contact
-    mail to: contact.email, bcc: ENV["ACTION_MAILER_USER"], subject: "お問い合わせについて【自動送信】"
+    mail to: @contact.email, bcc: ENV["MAIL_ADDRESS"], subject: "お問い合わせについて【自動送信】"
   end
 end
