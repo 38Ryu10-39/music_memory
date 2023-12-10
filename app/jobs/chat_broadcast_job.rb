fe921a28ef4641d84e7d99a0bf587a2b8ef22e99
38Ryu10-39 <keshi-.-tp_38@docomo.ne.jp>
2023-12-11 02:29:16 +0900

    Fix: チャット機能に関して修正とレイアウトの変更を行いました

--- app/jobs/chat_broadcast_job.rb
+++ app/jobs/chat_broadcast_job.rb
@@ -1,13 +1,13 @@
 class ChatBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(chat, options = {})
-    ActionCable.server.broadcast "room_channel", { chat: render_chat(chat, options), current_user_id: options[:current_user_id] }
+  def perform(chat)
+    ActionCable.server.broadcast "room_channel", { chat: render_chat(chat) }
   end
 
   private
 
-  def render_chat(chat, options)
-    ApplicationController.renderer.render(partial: 'chats/chat', locals: { chat: chat, current_user_id: options[:current_user_id] })
+  def render_chat(chat)
+    ApplicationController.renderer.render(partial: 'chats/chat', locals: { chat: chat })
   end
 end
